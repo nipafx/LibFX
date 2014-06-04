eb4c0a2c9c1ae7de508ed2a9a0ff0427df2bb96e
Nicolai Parlog <nipa@posteo.org>
2014-06-04 11:22:21 +0200

    Added a simple demo for nested properties.

--- test/org/codefx/AllTests.java
+++ test/org/codefx/AllTests.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
-	_AllNestingTests.class,
+		_AllNestingTests.class,
 })
 public class AllTests {
 	// no body needed
