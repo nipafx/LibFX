771c0db2737068b5af3ad563ec55a5f44e715d4d
nicolaiparlog <nipa@codefx.org>
2014-10-05 15:07:44 +0200

    Shortened package-info of 'controls' to the essentials.

--- src/main/java/org/codefx/libfx/control/package-info.java
+++ src/main/java/org/codefx/libfx/control/package-info.java
@@ -1,40 +1,19 @@
 /**
  * <p>
  * This package provides functionality to make using a {@link javafx.scene.control.Control Control}'s
- * {@link javafx.scene.control.Control#getProperties() propertyMap} easier. As such it's main use will be to creators of
+ * {@link javafx.scene.control.Control#getProperties() propertyMap} easier. As such its main use will be to creators of
  * controls.
  * <p>
- * <h2>Property Map</h2> Quick introduction to the property map.
- * <p>
- * <h3>Model View Controler</h3> A control should always be split into three classes:
+ * <h2>Listening to the Property Map</h2> In order to use the property map, a control has to create a listener which
+ * does these things:
  * <ul>
- * <li>the control, which handles the state (the model in MVC)
- * <li>the skin, which handles the layout (the view in MVC)
- * <li>the behavior, which handles how the control reacts to user input (the controller in MVC)
+ * <li>identify the correct key
+ * <li>check whether the value is of the correct type and cast it
+ * <li>process the value
+ * <li>remove the value so when the same value is added again, the listener notices that
  * </ul>
- * <p>
- * <h3>Interaction in MVC</h3> As such, the behavior and skin interact with the control as any other class would. This
- * means that they have no privilege when it comes to manipulating its state. But sometimes the control's state has to
- * reflect something that depends on one of these classes and which users of the control should be able to observe but
- * not influence.
- * <p>
- * (Think about a status flag that is true as long as there is an ongoing UI interaction with the control like dragging
- * something across it. This must be set by the behavior but should be read-only to the user of the control.)
- * <p>
- * <h3>Property Map</h3> For such occasions, a control's property map provides a "back door" into the control. It is
- * nothing special and can still be used by any other class. But a control does typically not document this interface
- * and it should be regarded as unofficial and mutable.
- * <p>
- * The property map is an {@link javafx.collections.ObservableMap ObservableMap (of Object, Object)} and can be edited
- * like this:
- *
- * <pre>
- * Button button = new Button();
- * button.getProperties().put("SomeKey", "SomeValue");
- * </pre>
- * It is then up to the control to listen to those changes and do something with them. While implementing such a
- * listener is not difficult, some details have to be considered. This makes the code a little lengthy and hinders
- * readability.
+ * While implementing such a listener is not difficult, some details have to be considered. This makes the code a little
+ * lengthy and hinders readability while at the same time repeating the same pattern over and over.
  * <p>
  * <h2>ControlPropertyListener</h2> This package provides usability functions to create such a listener in a concise and
  * readable way (this code would be inside a control):
