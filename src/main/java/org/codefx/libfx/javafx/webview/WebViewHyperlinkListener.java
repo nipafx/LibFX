c289d92b9f7d66f593f537d4e1738d4f08d525b0
nicolaiparlog <nipa@codefx.org>
2014-10-16 18:28:23 +0200

    Added adapter methods to 'WebViewHyperlinkListener'.

--- src/main/java/org/codefx/libfx/javafx/webview/WebViewHyperlinkListener.java
+++ src/main/java/org/codefx/libfx/javafx/webview/WebViewHyperlinkListener.java
@@ -1,5 +1,7 @@
 package org.codefx.libfx.javafx.webview;
 
+import java.util.function.Function;
+
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
@@ -16,12 +18,46 @@ import javax.swing.event.HyperlinkListener;
 public interface WebViewHyperlinkListener {
 
 	/**
+	 * Adapts the specified (Swing) hyperlink listener to a web view hyperlink listener.
+	 *
+	 * @param listener
+	 *            the {@link HyperlinkListener} to adapt
+	 * @param cancel
+	 *            a {@link Function} which checks for the specified event whether it should be canceled
+	 * @return a {@link WebViewHyperlinkListener}
+	 */
+	static WebViewHyperlinkListener fromHyperlinkListener(HyperlinkListener listener,
+			Function<HyperlinkEvent, Boolean> cancel) {
+		return event -> {
+			listener.hyperlinkUpdate(event);
+			return cancel.apply(event);
+		};
+	}
+
+	/**
+	 * Adapts the specified (Swing) hyperlink listener to a web view hyperlink listener.
+	 *
+	 * @param listener
+	 *            the {@link HyperlinkListener} to adapt
+	 * @param cancel
+	 *            whether the created listener should cancel every event
+	 * @return a {@link WebViewHyperlinkListener}
+	 */
+	static WebViewHyperlinkListener fromHyperlinkListener(HyperlinkListener listener, boolean cancel) {
+		return event -> {
+			listener.hyperlinkUpdate(event);
+			return cancel;
+		};
+	}
+
+	/**
 	 * Called when a hypertext link is updated.
 	 *
 	 * @param event
 	 *            the event responsible for the update
-	 * @return true if the event is to be canceled
+	 * @return whether the event should be canceled; if more than one listener is called on a single event, their return
+	 *         values are "ored".
 	 */
-	public boolean hyperlinkUpdate(HyperlinkEvent event);
+	boolean hyperlinkUpdate(HyperlinkEvent event);
 
 }
