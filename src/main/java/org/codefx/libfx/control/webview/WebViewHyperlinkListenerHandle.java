f6df87eb34f72db1e962641699fbf7689d0e7d5b
nicolaiparlog <nipa@codefx.org>
2014-10-17 18:37:12 +0200

    'WebViewHyperlinkListener' can now be detached.

--- src/main/java/org/codefx/libfx/control/webview/WebViewHyperlinkListenerHandle.java
+++ src/main/java/org/codefx/libfx/control/webview/WebViewHyperlinkListenerHandle.java
@@ -1,6 +1,9 @@
 package org.codefx.libfx.control.webview;
 
 import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import javafx.application.Platform;
 import javafx.beans.value.ObservableValue;
@@ -10,6 +13,7 @@ import javafx.scene.web.WebView;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkEvent.EventType;
 
+import org.codefx.libfx.concurrent.when.ExecuteAlwaysWhen;
 import org.codefx.libfx.dom.DomEventType;
 import org.codefx.libfx.dom.EventTransformer;
 import org.w3c.dom.NodeList;
@@ -18,7 +22,7 @@ import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 
 /**
- * A handle on the created listener which allows to {@link #attach()} and TODO detach it.
+ * A handle on the created listener which allows to {@link #attach()} and {@link #detach()} it.
  */
 class WebViewHyperlinkListenerHandle {
 
@@ -58,6 +62,14 @@ class WebViewHyperlinkListenerHandle {
 	 * Transforms DOM {@link Event}s to {@link HyperlinkEvent}s.
 	 */
 	private final EventTransformer eventTransformer;
+
+	/**
+	 * Executes {@link #attachListenerInApplicationThread()} each time the web view's load worker changes its state to
+	 * {@link State#SUCCEEDED SUCCEEDED}.
+	 * <p>
+	 * The executer is only present while the listener is attached.
+	 */
+	private Optional<ExecuteAlwaysWhen<State>> attachWhenLoadSucceeds;
 
 	// #end ATTRIBUTES
 
@@ -105,11 +117,11 @@ class WebViewHyperlinkListenerHandle {
 	 */
 	private void attachInApplicationThreadEachTimeLoadSucceeds() {
 		ObservableValue<State> webWorkerState = webView.getEngine().getLoadWorker().stateProperty();
-		webWorkerState.addListener((observable, oldState, newState) -> {
-			if (newState != State.SUCCEEDED)
-				return;
-			attachInApplicationThread();
-		});
+		attachWhenLoadSucceeds = Optional.of(new ExecuteAlwaysWhen<>(
+				webWorkerState,
+				state -> state == State.SUCCEEDED,
+				state -> attachListenerInApplicationThread()));
+		attachWhenLoadSucceeds.get().executeWhen();
 	}
 
 	/**
@@ -117,28 +129,84 @@ class WebViewHyperlinkListenerHandle {
 	 * <p>
 	 * Must be called in JavaFX application thread.
 	 */
-	private void attachInApplicationThread() {
+	private void attachListenerInApplicationThread() {
+		BiConsumer<EventTarget, String> addListener =
+				(eventTarget, eventType) -> eventTarget.addEventListener(eventType, domEventListener, false);
+		onEachLinkForEachManagedEventType(addListener);
+	}
+
+	// #end ATTACH
+
+	// #region DETACH
+
+	/**
+	 * Detaches the {@link WebViewHyperlinkListener} from the {@link WebView}.
+	 */
+	public void detach() {
+		if (Platform.isFxApplicationThread())
+			detachInApplicationThread();
+		else
+			Platform.runLater(() -> detachInApplicationThread());
+	}
+
+	/**
+	 * Detaches the {@link #domEventListener} from the {@link #webView} and cancels and resets
+	 * {@link #attachWhenLoadSucceeds}.
+	 * <p>
+	 * Must be called in JavaFX application thread.
+	 */
+	private void detachInApplicationThread() {
+		attachWhenLoadSucceeds.ifPresent(attachWhen -> attachWhen.cancel());
+		attachWhenLoadSucceeds = Optional.empty();
+
+		// it suffices to remove the listener if the worker state is on SUCCEEDED;
+		// because when the view is currently loading, the canceled 'attachWhen' will not re-add the listener
+		State webWorkerState = webView.getEngine().getLoadWorker().getState();
+		if (webWorkerState == State.SUCCEEDED) {
+			BiConsumer<EventTarget, String> removeListener =
+					(eventTarget, eventType) -> eventTarget.removeEventListener(eventType, domEventListener, false);
+			onEachLinkForEachManagedEventType(removeListener);
+		}
+	}
+
+	// #end DETACH
+
+	// #region COMMON MANAGEMENT METHODS
+
+	/**
+	 * Executes the specified function on each link in the {@link #webView}'s current document for each
+	 * {@link DomEventType} for which {@link #manageListenerForEventType(DomEventType)} returns true.
+	 * <p>
+	 * Must be called in JavaFX application thread.
+	 *
+	 * @param manageListener
+	 *            a {@link BiConsumer} which acts on a link and a DOM event type
+	 */
+	private void onEachLinkForEachManagedEventType(BiConsumer<EventTarget, String> manageListener) {
 		NodeList domNodeList = webView.getEngine().getDocument().getElementsByTagName("a");
 		for (int i = 0; i < domNodeList.getLength(); i++) {
 			EventTarget domTarget = (EventTarget) domNodeList.item(i);
-			for (DomEventType domEventType : DomEventType.values())
-				addDomListenerToTarget(domTarget, domEventType);
+			onLinkForEachManagedEventType(domTarget, manageListener);
 		}
 	}
 
 	/**
-	 * Adds the {@link #domEventListener} to the specified DOM event target if the specified DOM event type passes the
-	 * {@link #eventTypeFilter}.
+	 * Executes the specified function on the specified link for each {@link DomEventType} for which
+	 * {@link #manageListenerForEventType(DomEventType)} returns true.
+	 * <p>
+	 * Must be called in JavaFX application thread.
 	 *
-	 * @param domTarget
-	 *            the {@link EventTarget} to which the listener might be added
-	 * @param domEventType
-	 *            the {@link DomEventType} for which a listener might be added
+	 * @param link
+	 *            The {@link EventTarget} with which {@code manageListener} will be called
+	 * @param manageListener
+	 *            a {@link BiConsumer} which acts on a link and a DOM event type
 	 */
-	private void addDomListenerToTarget(EventTarget domTarget, DomEventType domEventType) {
-		boolean addListenerForEventType = mustAddListenerForEventType(domEventType);
-		if (addListenerForEventType)
-			domTarget.addEventListener(domEventType.getDomName(), domEventListener, false);
+	private void onLinkForEachManagedEventType(EventTarget link, BiConsumer<EventTarget, String> manageListener) {
+		Consumer<DomEventType> manageListenerForType =
+				domEventType -> manageListener.accept(link, domEventType.getDomName());
+		Stream.of(DomEventType.values())
+				.filter(this::manageListenerForEventType)
+				.forEach(manageListenerForType);
 	}
 
 	/**
@@ -149,7 +217,7 @@ class WebViewHyperlinkListenerHandle {
 	 * @return true if the DOM event type has a representation as an hyperlink event type and is not filtered out; false
 	 *         otherwise
 	 */
-	private boolean mustAddListenerForEventType(DomEventType domEventType) {
+	private boolean manageListenerForEventType(DomEventType domEventType) {
 		boolean domEventTypeHasRepresentation = domEventType.toHyperlinkEventType().isPresent();
 		if (!domEventTypeHasRepresentation)
 			return false;
@@ -161,7 +229,7 @@ class WebViewHyperlinkListenerHandle {
 		return domEventType.toHyperlinkEventType().get() == eventTypeFilter.get();
 	}
 
-	// #end ATTACH
+	// #end COMMON MANAGEMENT METHODS
 
 	// #region PROCESS EVENT
 
