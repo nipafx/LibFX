f3e8e5255f1140bee02e832b14df1c0dc5bdf2aa
nicolaiparlog <nipa@codefx.org>
2014-10-20 07:48:49 +0200

    Created test hierarchy level for controls.

--- src/test/java/org/codefx/libfx/AllTests.java
+++ src/test/java/org/codefx/libfx/AllTests.java
@@ -1,7 +1,7 @@
 package org.codefx.libfx;
 
 import org.codefx.libfx.concurrent._AllConcurrentTests;
-import org.codefx.libfx.control.properties._AllPropertiesTests;
+import org.codefx.libfx.control._AllControlTests;
 import org.codefx.libfx.dom._AllDomTests;
 import org.codefx.libfx.nesting._AllNestingTests;
 import org.codefx.libfx.serialization._AllSerializationTests;
@@ -15,9 +15,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
 		_AllConcurrentTests.class,
+		_AllControlTests.class,
 		_AllDomTests.class,
 		_AllNestingTests.class,
-		_AllPropertiesTests.class,
 		_AllSerializationTests.class,
 })
 public class AllTests {
---
+++ src/test/java/org/codefx/libfx/control/_AllControlTests.java
@@ -0,0 +1,17 @@
+package org.codefx.libfx.control;
+
+import org.codefx.libfx.control.properties._AllPropertiesTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Runs all tests in this package and its subpackages.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+		_AllPropertiesTests.class,
+})
+public class _AllControlTests {
+	// no body needed
+}
