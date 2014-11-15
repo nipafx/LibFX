c6390c6719be1acecca3a8e7c21f6a0c648a1c40
nicolaiparlog <nipa@codefx.org>
2014-11-15 17:04:18 +0100

    Factory class 'ListenerHandles' creates handles for 'Observable' and 'ObservableValue'.

--- src/main/java/org/codefx/libfx/listener/handle/ListenerHandles.java
+++ src/main/java/org/codefx/libfx/listener/handle/ListenerHandles.java
@@ -1,7 +1,12 @@
 package org.codefx.libfx.listener.handle;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
 /**
- * Utility class for functionality surrounding {@link ListenerHandle}s.
+ * Factory class for functionality surrounding {@link ListenerHandle}s.
  */
 public class ListenerHandles {
 
@@ -26,8 +31,84 @@ public class ListenerHandles {
 	 *            the listener which will be added to the {@code observable}
 	 * @return a {@link ListenerHandleBuilder} for a {@code ListenerHandle}.
 	 */
-	public static <O, L> ListenerHandleBuilder<O, L> from(O observable, L listener) {
+	public static <O, L> ListenerHandleBuilder<O, L> buildFor(O observable, L listener) {
 		return ListenerHandleBuilder.from(observable, listener);
+	}
+
+	// Observable + InvalidationListener
+
+	/**
+	 * Ands the specified listener to the specified observable and returns a handle for the combination.
+	 *
+	 * @param observable
+	 *            the {@link Observable} to which the {@code invalidationListener} will be added
+	 * @param invalidationListener
+	 *            the {@link InvalidationListener} which will be added to the {@code observable}
+	 * @return a {@link ListenerHandle} for the specified arguments; the listener is initially attached
+	 */
+	public static ListenerHandle create(Observable observable, InvalidationListener invalidationListener) {
+		ListenerHandle handle = createDetached(observable, invalidationListener);
+		handle.attach();
+		return handle;
+	}
+
+	/**
+	 * Creates a listener handle for the specified observable and listener. The listener is not yet attached!
+	 *
+	 * @param observable
+	 *            the {@link Observable} to which the {@code invalidationListener} will be added
+	 * @param invalidationListener
+	 *            the {@link InvalidationListener} which will be added to the {@code observableValue}
+	 * @return a {@link ListenerHandle} for the specified arguments; the listener is initially detached
+	 */
+	public static ListenerHandle createDetached(Observable observable, InvalidationListener invalidationListener) {
+		return ListenerHandleBuilder
+				.from(observable, invalidationListener)
+				.onAttach((obs, listener) -> obs.addListener(listener))
+				.onDetach((obs, listener) -> obs.removeListener(listener))
+				.build();
+	}
+
+	// ObservableValue + ChangeListener
+
+	/**
+	 * Ands the specified listener to the specified observable and returns a handle for the combination.
+	 *
+	 * @param <T>
+	 *            the type of the value wrapped by the observable
+	 * @param observableValue
+	 *            the {@link ObservableValue} to which the {@code changeListener} will be added
+	 * @param changeListener
+	 *            the {@link ChangeListener} which will be added to the {@code observableValue}
+	 * @return a {@link ListenerHandle} for the specified arguments; the listener is initially attached
+	 */
+	public static <T> ListenerHandle create(
+			ObservableValue<T> observableValue, ChangeListener<? super T> changeListener) {
+
+		ListenerHandle handle = createDetached(observableValue, changeListener);
+		handle.attach();
+		return handle;
+	}
+
+	/**
+	 * Creates a listener handle for the specified observable and listener. The listener is not yet attached!
+	 *
+	 * @param <T>
+	 *            the type of the value wrapped by the observable
+	 * @param observableValue
+	 *            the {@link ObservableValue} to which the {@code changeListener} will be added
+	 * @param changeListener
+	 *            the {@link ChangeListener} which will be added to the {@code observableValue}
+	 * @return a {@link ListenerHandle} for the specified arguments; the listener is initially detached
+	 */
+	public static <T> ListenerHandle createDetached(
+			ObservableValue<T> observableValue, ChangeListener<? super T> changeListener) {
+
+		return ListenerHandleBuilder
+				.from(observableValue, changeListener)
+				.onAttach((observable, listener) -> observable.addListener(listener))
+				.onDetach((observable, listener) -> observable.removeListener(listener))
+				.build();
 	}
 
 }
