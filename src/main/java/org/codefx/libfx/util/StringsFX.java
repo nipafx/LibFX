77752ee49258dceb933bc52159540d99464f2bbb
nipa <nipa@codefx.org>
2015-10-10 00:56:07 +0200

    Document 'StringFX'

--- src/main/java/org/codefx/libfx/util/StringsFX.java
+++ src/main/java/org/codefx/libfx/util/StringsFX.java
@@ -1,6 +1,10 @@
 package org.codefx.libfx.util;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -11,22 +15,72 @@ public class StringsFX {
 
 	public static <T> String join(
 			Iterable<T> items, Function<T, String> toString,
-			String prefix, String delimiter, String suffix, String emptyValue) {
-		return join(StreamFX.sequentialStream(items), toString, prefix, delimiter, suffix, emptyValue);
+			String delimiter, String prefix, String suffix, String emptyValue) {
+		return join(StreamFX.stream(items), toString, delimiter, prefix, suffix, emptyValue);
 	}
 
-    public static <T> String join(
+	/**
+	 * Joins the strings to which the objects from the specified {@link Collection} are mapped.
+	 * <p/>
+	 * See {@link StreamFX#joining(CharSequence, CharSequence, CharSequence, CharSequence) StreamFX.joining} for how
+	 * exactly the strings are joined.
+	 *
+	 * @param <T>
+	 * 		the type of the elements contained in the specified collection
+	 * @param items
+	 * 		the joined items
+	 * @param toString
+	 * 		maps the items to strings; could be as simple as {@code Object::toString}
+	 * @param delimiter
+	 * 		inserted between two values
+	 * @param prefix
+	 * 		the first characters of the created string; followed by the first value
+	 * @param suffix
+	 * 		the last characters of the created string; follows the last value
+	 * @param emptyValue
+	 * 		used if the string is empty
+	 *
+	 * @return the joined string
+	 */
+	public static <T> String join(
 			Collection<T> items, Function<T, String> toString,
-			String prefix, String delimiter, String suffix, String emptyValue) {
-		return join(items.stream(), toString, prefix, delimiter, suffix, emptyValue);
+			String delimiter, String prefix, String suffix, String emptyValue) {
+		requireNonNull(items, "The argument 'items' must not be null.");
+		return join(items.stream(), toString, delimiter, prefix, suffix, emptyValue);
 	}
 
-    public static <T> String join(
+	/**
+	 * Joins the strings to which the objects from the specified {@link Stream} are mapped.
+	 * <p/>
+	 * See {@link StreamFX#joining(CharSequence, CharSequence, CharSequence, CharSequence) StreamFX.joining} for how
+	 * exactly the strings are joined.
+	 *
+	 * @param <T>
+	 * 		the type of the elements contained in the specified stream
+	 * @param items
+	 * 		the joined items
+	 * @param toString
+	 * 		maps the items to strings; could be as simple as {@code Object::toString}
+	 * @param delimiter
+	 * 		inserted between two values
+	 * @param prefix
+	 * 		the first characters of the created string; followed by the first value
+	 * @param suffix
+	 * 		the last characters of the created string; follows the last value
+	 * @param emptyValue
+	 * 		used if the string is empty
+	 *
+	 * @return the joined string
+	 */
+	public static <T> String join(
 			Stream<T> items, Function<T, String> toString,
-			String prefix, String delimiter, String suffix, String emptyValue) {
+			String delimiter, String prefix, String suffix, String emptyValue) {
+		requireNonNull(items, "The argument 'items' must not be null.");
+		requireNonNull(toString, "The argument 'toString' must not be null.");
+
+		// 'StreamFX.joining' tests all other arguments for null
+
 		return items.map(toString).collect(StreamFX.joining(delimiter, prefix, suffix, emptyValue));
 	}
-
-    // TODO consider adding
 
 }
